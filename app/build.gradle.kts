e5170cf50b9906c902a3bf1c7894a55deea48d0a
Hùng Nguyễn <vanhung211104@gmail.com>
2025-04-17 22:05:20 +0700

    Login, SignUp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id ("com.google.gms.google-services")
 }
 
 android {
@@ -37,6 +38,23 @@ android {
 }
 
 dependencies {
+
+    implementation ("com.google.android.gms:play-services-auth:20.4.1")
+    /**firebase dependencies*/
+    implementation ("com.google.firebase:firebase-auth:21.1.0")
+    implementation ("com.google.firebase:firebase-database:20.1.0")
+    implementation (platform("com.google.firebase:firebase-bom:31.2.3"))
+    implementation ("com.google.firebase:firebase-database-ktx")
+    implementation ("com.google.firebase:firebase-firestore:24.4.5")
+    implementation ("com.google.firebase:firebase-storage:20.1.0")
+    implementation ("com.firebaseui:firebase-ui-database:8.0.1")
+
+    implementation("com.github.TutorialsAndroid:GButton:v1.0.19")
+    implementation("com.github.bumptech.glide:glide:4.14.2")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.14.2")
+
+    // cloudinary
+    implementation("com.cloudinary:cloudinary-android:3.0.2")
 
     implementation(libs.appcompat)
     implementation(libs.material)
